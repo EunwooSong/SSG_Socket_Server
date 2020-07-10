2a9daa6637b2d2e1acf43a0005a399e7da66f79c
HADMARINE <tsx@kakao.com>
2020-07-10 09:27:46 +0900

    Update connectDB.ts
    fuck, eunwoo

--- src/Module/connectDB.ts
+++ src/Module/connectDB.ts
@@ -1,12 +1,36 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 require('dotenv').config();
 
-const MONGO_URL = process.env.MONGO_URI || 'mongodb://localhost:27017/SimpleShootingGame_Server';
+interface Auth {
+  user: string;
+  pass: string;
+}
+
+const MONGO_URL = 'mongodb://' + process.env.DB_HOST;
 const env = process.env.NODE_ENV || 'development';
 
-let mongoURL: any = MONGO_URL;
-if (env !== 'production') mongoURL += `_${env}`;
-if (env === 'development') {
-    mongoose.set('debug', true);
+if (!process.env.DB_USER || !process.env.DB_PASS) {
+  throw new Error('DB AUTH INFO NOT PROVIDED');
 }
-module.exports = () => mongoose.connect(mongoURL);
+
+const auth: Auth = {
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
+};
+
+const mongoURL: any = MONGO_URL;
+let dbName: any = process.env.DB_NAME;
+
+if (env !== 'production') dbName += `_${env}`;
+if (env === 'development') {
+  mongoose.set('debug', true);
+}
+
+export default (): Promise<typeof mongoose> =>
+  mongoose.connect(mongoURL, {
+    ...auth,
+    dbName,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
